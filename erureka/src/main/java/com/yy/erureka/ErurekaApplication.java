bf6f8366435273284d5a6785f02e41120e495072
youyu <youyu3@outlook.com>
2018-07-13 18:40:48 +0800

    spring cloud eureka 实现注册服务功能

---
+++ erureka/src/main/java/com/yy/erureka/ErurekaApplication.java
@@ -0,0 +1,12 @@
+package com.yy.erureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErurekaApplication.class, args);
+    }
+}
