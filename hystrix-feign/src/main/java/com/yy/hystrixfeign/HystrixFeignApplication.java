59e5afdb440028ceb47876fb04031b24fccb40f1
youyu <youyu3@outlook.com>
2018-07-18 17:55:40 +0800

    feign hystrix 加入dashboard可视化界面，单节点服务统计demo

--- hystrix-feign/src/main/java/com/yy/hystrixfeign/HystrixFeignApplication.java
+++ hystrix-feign/src/main/java/com/yy/hystrixfeign/HystrixFeignApplication.java
@@ -1,18 +1,38 @@
 package com.yy.hystrixfeign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class HystrixFeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixFeignApplication.class, args);
+    }
+
+    /**
+     * springcloud 2.0.0版本之后dashboard需要重新配置一个servlet,此例为单节点的监控
+     * 且地址必须配置为/actuator/hystrix.stream，暂时还没搞清楚为什么，后续跟踪。
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
